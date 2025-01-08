c8517226afb7d0660ffbfbe72c590477004cbc5b
SteveBenz <s8878992@hotmail.com>
2025-01-08 10:36:34 -0800

    Fix object names and make the capitalization align with stock objects

--- QuestableTractor/ObjectIds.cs
+++ QuestableTractor/ObjectIds.cs
@@ -42,7 +42,7 @@ namespace NermNermNerm.Stardew.QuestableTractor
             };
             addQuestItem(
                 BustedEngine,
-                L("funky looking engine that doesn't work"),
+                L("Funky looking engine that doesn't work"),
                 L("Sebastian pulled this off of the rusty tractor.  I need to find someone to fix it."),
                 10);
             addQuestItem(
@@ -52,42 +52,42 @@ namespace NermNermNerm.Stardew.QuestableTractor
                 0);
             addQuestItem(
                 BustedScythe,
-                L("core of the harvesting attachment for the tractor"),
+                L("Core of the harvesting attachment for the tractor"),
                 L("This looks like it was a tractor attachment for harvesting crops, but it doesn't seem to be all together."),
                 12);
             addQuestItem(
                 WorkingScythe,
-                L("harvesting attachment for the tractor"),
+                L("Harvesting attachment for the tractor"),
                 L("Just need to bring this to the tractor garage to be able to use it with the tractor!"),
                 2);
             addQuestItem(
                 ScythePart1,
-                L("crop shakerlooser"),
+                L("Crop shakerlooser"),
                 L("One of the missing parts for the scythe attachment"),
                 7);
             addQuestItem(
                 ScythePart2,
-                L("shiny sprocket"),
+                L("Shiny sprocket"),
                 L("One of the missing parts for the scythe attachment"),
                 6);
             addQuestItem(
                 BustedWaterer,
-                L("broken watering attachment for the tractor"),
+                L("Broken watering attachment for the tractor"),
                 L("This looks like it was a tractor attachment for watering crops.  Sure hope somebody can help me get it working again, watering can really be a drag."),
                 20);
             addQuestItem(
                 WorkingWaterer,
-                L("watering attachment for the tractor"),
+                L("Watering attachment for the tractor"),
                 L("The watering attachment for the tractor - it needs to be brought back to the tractor garage."),
                 1);
             addQuestItem(
                 BustedLoader,
-                L("bent up and rusty front-end loader for the tractor"),
+                L("Bent up and rusty front-end loader for the tractor"),
                 L("This was the front-end loader attachment (for picking up rocks and sticks), but it's all bent up and rusted through in spots.  It needs to be fixed to be usable."),
                 18);
             addQuestItem(
                 WorkingLoader,
-                L("front-end loader attachment for my tractor"),
+                L("Front-end loader attachment for my tractor"),
                 L("This will allow me to clear rocks and sticks on my farm.  It needs to go into the tractor garage so I can use it."),
                 9);
             addQuestItem(
@@ -97,17 +97,17 @@ namespace NermNermNerm.Stardew.QuestableTractor
                 4);
             addQuestItem(
                 DisguisedShoe,
-                L("cleverly repackaged pair of shoes"),
+                L("Cleverly repackaged pair of shoes"),
                 L("Alex's old shoes, cleverly dyed.  Nobody will ever know."),
                 11);
             addQuestItem(
                 BustedSeeder,
-                L("broken fertilizer and seeder"),
+                L("Broken fertilizer and seeder"),
                 L("The fertilizer and seed spread for the old tractor.  It needs a good bit of fiddling to make work."),
                 8);
             addQuestItem(
                 WorkingSeeder,
-                L("fertilizer and seed Seeder attachment for the tractor"),
+                L("Fertilizer and seeder attachment for the tractor"),
                 L("Just needs to be brought back to the garage to use it on the tractor."),
                 14);
         }
